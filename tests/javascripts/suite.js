c26ec2677331d5b822b458fed2540697adaaf275
Jason Benterou <jason@cloudflare.com>
2011-03-07 13:23:05 -0800

    Upgrate test suite interface: add direct links to tests

--- tests/javascripts/suite.js
+++ tests/javascripts/suite.js
@@ -2,8 +2,10 @@
     
     $(function() {
         
-        var normalView = $('#NormalView'),
-            djsView = $('#DJSView'),
+        var normalView = $('#NormalView iframe'),
+            normalViewLink = $('#NormalView .test-link'),
+            djsView = $('#DJSView iframe'),
+            djsViewLink = $('#DJSView .test-link'),
             lastTarget;
         
         normalView.attr('src', '/tests/blank.html');
@@ -14,10 +16,16 @@
             function(event) {
                 
                 var target = $(event.currentTarget),
-                    test = target.attr('rel');
+                    test = target.attr('rel'),
+                    testName = target.text();
                 
                 normalView.attr('src', '/tests/' + test + '.html');
+                normalViewLink.attr('href', '/tests/' + test + '.html');
+                normalViewLink.text(testName);
+
                 djsView.attr('src', '/tests/' + test + '-djs.html');
+                djsViewLink.attr('href', '/tests/' + test + '-djs.html');
+                djsViewLink.text(testName);
                 
                 if(lastTarget) {
                     
@@ -25,6 +33,8 @@
                 }
                 
                 lastTarget = target.addClass('active');
+
+                $('#TestContainer').addClass('active');
             }
         );
     });
