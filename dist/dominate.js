d71e351e670c8698ca391ecb71163e9554e6e207
Jason Benterou <jason@cloudflare.com>
2011-03-22 10:53:01 -0700

    Use Image() preloading strategy if browser disallows Object preloading by URL file extension

--- dist/dominate.js
+++ dist/dominate.js
@@ -1043,9 +1043,12 @@ exports.DomUtils = DomUtils;
         ModernIE: window.navigator.userAgent.indexOf('MSIE 8') !== -1 || window.navigator.userAgent.indexOf('MSIE 9') !== -1,
         IE: window.navigator.userAgent.indexOf('MSIE') !== -1,
         Chrome: window.navigator.userAgent.indexOf('Chrome/') !== -1,
+        Safari: window.navigator.userAgent.indexOf('Chrome/') !== -1,
         Opera: window.navigator.userAgent.indexOf('Opera') !== -1,
         Webkit: window.navigator.userAgent.indexOf('AppleWebKit') !== -1,
-        Firefox: window.navigator.userAgent.indexOf('Firefox/') !== -1
+        Firefox: window.navigator.userAgent.indexOf('Firefox/') !== -1,
+        Macintosh: window.navigator.userAgent.indexOf('Macintosh') !== -1,
+        Windows: window.navigator.userAgent.indexOf('Windows') !== -1
     },
 
 /*
@@ -1190,10 +1193,58 @@ exports.DomUtils = DomUtils;
  *
  * Return the class name of an object
  */
-
     DJSUtil.getClass = function(obj) {
 
         return Object.prototype.toString.call(obj);
+    };
+
+/*
+ * DJSUtil.parseUrl
+ *
+ * Parse the URL into parts
+ *
+ * 0 URL
+ * 1 protocol + domain + port
+ * 2 protocol
+ * 3 domain + port
+ * 4 domain
+ * 5 port
+ * 6 path
+ * 7 query string
+ * 8 query string initial char (? or &)
+ *
+ * e.g. "http://example.com/files/filename.js?xy=z" --> [
+    "http://example.com/files/filename.js?xy=z",    // url
+    "http://example.com",   // protocol + domain + port
+    "http://",              // protocol
+    "example.com",          // domain + port
+    undefined,              // no port
+    "example.com",          // domain
+    "/files/filename.js",   // path
+    "?xy=z",                // query string
+    "?"                     // query string initial char
+    ]
+ */
+    DJSUtil.parseUrl = function(url) {
+
+        return url && url.match(/^(([^:\/]+:\/\/)(([^:\/]+)(:[^:\/]+)?))?([^?&]*)((\?|&).*)?$/);
+    };
+
+/*
+ * DJSUtil.getPathExtension
+ *
+ * Return the file extension given a URL
+ *
+ * e.g. "http://example.com/files/filename.js?xy=z" --> "js"
+ */
+    DJSUtil.getPathExtension = function(url) {
+
+        var parts = DJSUtil.parseUrl(url),
+            pathFragments = parts[6] ? parts[6].split('/') : [''],
+            lastPath = pathFragments[pathFragments.length - 1],
+            dotParts = lastPath.split && lastPath.split('.');
+
+        return dotParts[dotParts.length - 1];
     };
 
 DJSUtil.setup();
@@ -3354,12 +3405,74 @@ DJSUtil.setup();
                     },
                     attachHandlers = function() {
                         
+                        // onload or onerror, so long as the HTTP request completed,
+                        // we must continue loading page scripts.
+                        // non-IE browsers will fire onerror when loading an asset with
+                        // non-image mimetype via the image strategy
                         precacheObject.onload = loadHandler;
-                        precacheObject.onerror = errorHandler;
+                        precacheObject.onerror = loadHandler;
                     },
                     detachHandlers = function() {
                      
                         precacheObject.onload = precacheObject.onerror = null;
+                    },
+
+                     // Use Image pre-caching strategy
+                     // Note that in Firefox, Chrome, and Safari, the image's
+                     // error callback will fire as the response mimetype
+                     // should be non-image, but that is acceptable as the
+                     // asset should be loaded into cache anyway
+                    preloadViaImage = function() {
+
+                        precacheObject = new Image();
+
+                        attachHandlers();
+                        
+                        precacheObject.src = self.src;
+                    },
+
+                    // Use Object pre-caching strategy
+                    // Chrome:Mac and Safari:Win refuse to load
+                    // assets with certain filenames using this scheme
+                    //
+                    // @target specifies insertion parent: i.e. document.body
+                    // vs head
+                    preloadViaObject = function(target) {
+
+                        precacheObject = document.createElement('object');
+                        
+                        attachHandlers();
+
+                        precacheObject.data = self.src;
+                        precacheObject.width = 0;
+                        precacheObject.height = 0;
+                        precacheObject.style.position = "absolute";
+                        precacheObject.style.left = "-999";
+
+                        target.appendChild(precacheObject);
+                    },
+                    preloadViaObjectInBody = function() {
+
+                        // in Chrome, sometimes document.body doesn't exist
+                        // but the task to create document.body is probably
+                        // waiting in the task queue.  So, stick this
+                        // task at the end of the task queue so when it resumes,
+                        // the document.body task will probably have run.
+                        // And if not, keep doing this until it shows up.
+                        // (inspired by http://bugs.jquery.com/ticket/4320,
+                        //  https://github.com/jquery/jquery/commit/262fcf7b7b919da1564509f621cf7480a5d5572b)
+                        if ('body' in document && document.body) {
+
+                            preloadViaObject(document.body);
+                        } else {
+
+                            setTimeout(preloadViaObjectInBody, 13);
+                        }
+                        
+                    };
+                    preloadViaObjectInHead = function() {
+
+                        preLoadViaObject(document.getElementsByTagName('head')[0]);
                     };
                 
                 DJSUtil.log('Precache-ing script at ' + self.src);
@@ -3367,27 +3480,28 @@ DJSUtil.setup();
                 if(DJSUtil.navigator.IE || DJSUtil.navigator.Opera) {
                     
                     // Precache the element as an Image...
-                    precacheObject = new Image();
+                    preloadViaImage();
 
-                    attachHandlers();
-                    
-                    precacheObject.src = self.src;
+                } else if(DJSUtil.navigator.Chrome && DJSUtil.navigator.Macintosh &&
+                    /php/i.test(DJSUtil.getPathExtension(self.src))) {
+
+                    // Chrome/Mac disallows preloading via Object
+                    // for asset URLs with path extension containing php
+                    preloadViaImage();
+
+                } else if(DJSUtil.navigator.Safari && DJSUtil.navigator.Windows &&
+                    /css/i.test(DJSUtil.getPathExtension(self.src))) {
+
+                    // Safari/Win disallows preloading via Object
+                    // for asset URLs with path extension containing css
+                    preloadViaImage();
+
+                } else if(DJSUtil.navigator.Firefox) {
+
+                    preloadViaObjectInHead();
                 } else {
 
-                    // Precache the element as an Object...
-                    var appendTarget = DJSUtil.navigator.Firefox ? document.getElementsByTagName('head')[0] : document.body;                        
-
-                    precacheObject = document.createElement('object'),
-                    
-                    attachHandlers();
-
-                    precacheObject.data = self.src;
-                    precacheObject.width = 0;
-                    precacheObject.height = 0;
-                    precacheObject.style.position = "absolute";
-                    precacheObject.style.left = "-999";
-
-                    appendTarget.appendChild(precacheObject);
+                    preloadViaObjectInBody();
                 }
             } else {
 
